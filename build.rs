4dea872ee64dce05b791cb4f9e0e5580092fc455
Andreas Rånman <rondreas@users.noreply.github.com>
2022-09-27 16:30:17 +0200

    Don't overwrite wrapper.h if exists, and allowlist types for minimal tests

--- build.rs
+++ build.rs
@@ -8,27 +8,32 @@ fn main() {
     // If this is missing, either set the environment variable or make a .cargo/config.toml with [env] variables,
     let include_path = env::var("LX_INCLUDE").expect("Missing Environment Variable: LX_INCLUDE");
 
-    // We then attempt to create the wrapper.h file,
-    let mut header = std::fs::File::create("wrapper.h").expect("Failed to create 'wrapper.h'");
+    let path = std::path::Path::new("wrapper.h");
 
-    // Create the wrapper.h from the headers,
-    let includes = std::fs::read_dir(&include_path).unwrap();  // This gets us an iterator with DirEntry objects in path,
+    if !path.exists() {
+        // We then attempt to create the wrapper.h file,
+        let mut header = std::fs::File::create(path).expect("Failed to create 'wrapper.h'");
 
-    // TODO: More concise method for getting all .h files as string
-    // For each DirEntry in includes, write_fmt the name for any headers found,
-    for include in includes {
-        let dir_entry = include.unwrap();
-        let path = dir_entry.path();
-        let filename = path.file_name().unwrap().to_str().expect("Failed to get path filename");
-        if path.extension().unwrap() == "h" {
-            if let Err(e) = writeln!(header, "#include \"{}\"", filename) {
-                println!("macro writeln failed: {}", e.to_string());
+        // Create the wrapper.h from the headers,
+        let includes = std::fs::read_dir(&include_path).unwrap();  // This gets us an iterator with DirEntry objects in path,
+
+        // TODO: More concise method for getting all .h files as string
+        // For each DirEntry in includes, write_fmt the name for any headers found,
+        for include in includes {
+            let dir_entry = include.unwrap();
+            let path = dir_entry.path();
+            let filename = path.file_name().unwrap().to_str().expect("Failed to get path filename");
+            if path.extension().unwrap() == "h" {
+                if let Err(e) = writeln!(header, "#include \"{}\"", filename) {
+                    println!("macro writeln failed: {}", e.to_string());
+                }
             }
         }
     }
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=build.rs");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -37,6 +42,8 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .allowlist_type("ILxModule")
+        .allowlist_type("ILxModuleID")
         .clang_arg(format!("--include-directory={}", include_path))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
